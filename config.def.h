752fa186c70ad428ba61eea0661820ebf6aeb5f7
Bark <kristoffer@nosslo.nu>
2022-04-30 23:37:19 +0200

    Add shortcut to switch mode

--- config.def.h
+++ config.def.h
@@ -65,11 +65,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_orange, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *lewdcmd[]  = { "/home/bark/dev/scripts/lewd", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,	             	XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,		XK_l,	   spawn,	   {.v = lewdcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },

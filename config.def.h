7ca1a7be2fb041d464361e4c1bdd0971ab7ee8f1
Bark <kristoffer@nosslo.nu>
2022-05-20 12:41:55 +0200

    Add full gaps support for tatami layout

--- config.def.h
+++ config.def.h
@@ -48,9 +48,9 @@ static const int lockfullscreen = 1;	/* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
+	{ "|+|",      tatami },
 //	{ "><>",      NULL },    /* no layout function means floating behavior */
 //	{ "[M]",      monocle },
-	{ "|+|",      tatami },
 };
 
 /* key definitions */
@@ -88,9 +88,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,						XK_x,      killclient,     {0} },
 	{ MODKEY,						XK_t,      setlayout,      {.v = &layouts[0]} },
-//	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-//	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,				XK_t,	   setlayout,	   {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,				XK_t,	   setlayout,	   {.v = &layouts[1]} },
+//	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+//	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
